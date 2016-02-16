a83f9e2a566d0502b1619bd7f0799fe447f489d2
erik tolentino and nic netzel <tolentino3575@gmail.com>
2016-02-15 16:36:58 -0800

    clean up spacing

--- src/LeetSpeak.php
+++ src/LeetSpeak.php
@@ -13,35 +13,28 @@
                 {
                 if ($character == "e" || $character == "E"){
                     array_push($new_array, "3");
-
                 }
                 elseif ($character == "o" || $character == "O")
                 {
                     array_push($new_array, "0");
-
                 }
                 elseif ($character == "L" )
                 {
                     array_push($new_array, "1");
-
                 }
                 elseif ( $character == "s" || $character == "S")
                 {
-
                     if ($i == 0 || $string_to_array[$i-1] == " "){
                         array_push($new_array, $character);
-
                     }
                     else {
                         array_push($new_array, "z");
-
                     }
                 }
 
                 else
                 {
                     array_push($new_array, $character);
-
                 }
                     ++$i;
             }
