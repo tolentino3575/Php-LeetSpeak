abd01346d3ee9b550c8f4eee0cd139c2a5d76480
erik tolentino and nic netzel <netzeln@gmail.com>
2016-02-15 16:21:38 -0800

    add simple interface

--- app/app.php
+++ app/app.php
@@ -1,6 +1,6 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/~~~~~~.php";
+    require_once __DIR__."/../src/LeetSpeak.php";
 
     session_start();
 
@@ -9,7 +9,16 @@
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path'=>__DIR__."/../views"
     ));
 
+    $app->get("/", function() use ($app){
+        return $app['twig']->render('index.html.twig');
+    });
 
+    $app->get("/LeetSpeak", function() use ($app){
+        $newLeetSpeak = new LeetSpeak;
+        $result = $newLeetSpeak->toLeetSpeak($_GET['words']);
+        return $app['twig']->render('index.html.twig', array('leet'=>$result));
+
+    });
 
     return $app;
  ?>
